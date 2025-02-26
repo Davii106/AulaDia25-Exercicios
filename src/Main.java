12f4962f31a692380e223ce802c0d906e0a7f4be
Davi Bessa Araujo <2400899@SALAS.AULAS>
2025-02-25 22:24:25 -0300

    Olá

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        //Implementando o boolean para verificar se a variável é true or false, no caso a Autoboot que é True
+        boolean Autoboot = true;
+        //Caso Autoboot seja verdadeiro ele irá printar
+        if (Autoboot) {
+            System.out.print("Autoboot");
+        //Como a boolean é verdadeira o else não será impresso
+        } else{
+            System.out.print("Decepticon");
+        }
+    }
+
+
+}
---
+++ src/Tabuada.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        //Pedindo ao usuário para atribuir qual a tabuada que o mesmo deseja visualizar
+        System.out.print("Digite qual a tabuada voce quer visualizar: ");
+        //Inserinando uma variável para o número que o usuário vai inserir
+        int numero = ler.nextInt();
+        //O for irá começar com o indíce 0 e irá até 0 10
+        for (int i = 0;i <= 10;i++){
+        //Este print irá imprimir a tabuada do número inicialemente atribuido pelo úsario, indo de 0 a 10
+            System.out.println(numero+"x" +i+ " = " + numero * i);
+        }
+    }
+}
